f7fa8bca4b4867691ae7d2bb90091ce7cc29fb7a
johnrau <luc.thang@gmail.com>
2023-12-16 20:49:39 +0700

    fix create folder

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 import time 
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
@@ -18,27 +18,33 @@ web = 'https://cafef.vn/'
 response = requests.get(web)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get(web)
-soup = BeautifulSoup(response.content,"html.parser")
+# soup = BeautifulSoup(response.content,"html.parser")
 driver.maximize_window()
+wait = WebDriverWait(driver, 100)
 
 def createFolder(mack):
-    path = 'C:/Users/TienLN/PRJ/Crawler_Projects'
-    os.mkdir(os.path.join(path,mack))
+    path = os.getcwd()
+    isExist = os.path.exists(os.path.join(path,mack))
+    if not isExist:
+        os.makedirs(os.path.join(path,mack))
     
 def searchMack(mack):
-
     #TÌM MÃ CHỨNG KHOÁN
     input_Ma = driver.find_element(By.XPATH,"//div[@id='CafeF_BoxSearchNew']//input[@id='CafeF_SearchKeyword_Company']")
     input_Ma.send_keys(mack)
-
     #BẤM NÚT "TÌM KIẾM"
     find_button = driver.find_element(By.XPATH,"//div[@id='CafeF_BoxSearchNew']//a")
     find_button.click()
- 
+
+    get_url = driver.current_url
     time.sleep(2)
+    return get_url
 
 def download_file():
-    urls = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr//td//a[@href]")))
+    print(' in download file : ', linkMack)
+    xpathLinks = (By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr//td//a[@href]")
+    urls = wait.until(EC.visibility_of_all_elements_located(xpathLinks))
+    print(urls)
     for urlBCTC in urls:
         link_BCTC = urlBCTC.get_attribute("href")
         # print(link_BCTC)
@@ -48,16 +54,17 @@ def download_file():
 
     with open(orginal_file_name, "wb") as pdfFile:
         pdfFile.write(response.content)
+
 for i in range(numDF):
     mack = DF['MACK'][i]
-    print(mack)
+    # print(mack)
 
     #TẠO FOLDER TRONG VÒNG FOR
     createFolder(mack)
 
     #TÌM MÃ CHỨNG KHOÁN 'MACK'
     linkMack = searchMack(mack)
-    print(linkMack)
+    # print(linkMack)
 
     #TÌM BCTC VÀ TẢI 
     download = download_file()
