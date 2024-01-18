5b82c6c6deeedbdc56284cacee0d5d55df5e7ce2
TienLuckyy296 <TienLN@TienLN-2906.com>
2024-01-18 21:55:05 +0700

    Before Mid-term test

--- Main.py
+++ Main.py
@@ -13,32 +13,54 @@ import os
 
 DF = pd.read_excel("FileBaoLoi.xlsx",sheet_name="test",usecols='A')
 numDF = len(DF.index)
-i=0
+i = int 
 web = 'https://cafef.vn/'
 response = requests.get(web)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get(web)
-# soup = BeautifulSoup(response.content,"html.parser")
-driver.maximize_window()
-wait = WebDriverWait(driver, 100)
+
+# driver.maximize_window()
+wait = WebDriverWait(driver, 10)
 
 def createFolder(mack):
     path = os.getcwd()
     isExist = os.path.exists(os.path.join(path,mack))
+    
     if not isExist:
         os.makedirs(os.path.join(path,mack))
-    
+
+    dir_path = os.path.join(path,mack)
+    return dir_path    
+
 def searchMack(mack):
-    #TÌM MÃ CHỨNG KHOÁN
     input_Ma = driver.find_element(By.XPATH,"//div[@id='CafeF_BoxSearchNew']//input[@id='CafeF_SearchKeyword_Company']")
     input_Ma.send_keys(mack)
-    #BẤM NÚT "TÌM KIẾM"
-    find_button = driver.find_element(By.XPATH,"//div[@id='CafeF_BoxSearchNew']//a")
+    
+    find_button = driver.find_element(By.XPATH,"//div[@id='CafeF_BoxSearchNew']//a[@class='bt_search sprite s-submit']")
     find_button.click()
 
-    get_url = driver.current_url
-    time.sleep(1000)
-    return get_url
+def findFiles():
+    find_BCTC_box = driver.find_element(By.XPATH,"//li[@id='liTabCongTy5CT']//a")
+    find_BCTC_box.click()
+    
+    xpathLinks = (By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr//td//a")
+    urls = wait.until(EC.visibility_of_all_elements_located(xpathLinks))
+    return urls   
+
+def nameCut():
+    for urlBCTC in findFiles():
+        link_BCTC = urlBCTC.get_attribute("href")
+        # print(link_BCTC)
+        orginal_file_name = link_BCTC.split('/')[7]
+        print(orginal_file_name)
+        response = requests.get(link_BCTC)
+
+        with open(os.path.join(createFolder(mack),orginal_file_name), "wb") as pdfFile:
+                pdfFile.write(response.content)
+
+def return_to_Homepage():
+    home_button = driver.find_element(By.XPATH,"//div[@id='menu_wrap']//li[@class='bt_home active']//a")
+    home_button.click()
 
 for i in range(numDF):
     mack = DF['MACK'][i]
@@ -51,16 +73,8 @@ for i in range(numDF):
     linkMack = searchMack(mack)
     # print(linkMack)
 
-    print(' in download file : ', linkMack)
-    xpathLinks = (By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr//td//a[@href]")
-    urls = wait.until(EC.visibility_of_all_elements_located(xpathLinks))
-    print(urls)
-    for urlBCTC in urls:
-        link_BCTC = urlBCTC.get_attribute("href")
-        # print(link_BCTC)
-        orginal_file_name = link_BCTC.split('/')[7]
-        print(orginal_file_name)
-        response = requests.get(link_BCTC)
+    findFiles()
 
-    with open(orginal_file_name, "wb") as pdfFile:
-        pdfFile.write(response.content)
+    nameCut()
+
+    return_to_Homepage()
