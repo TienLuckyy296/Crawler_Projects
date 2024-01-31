14917d0038b26e6516d970c57a45f3f1debde4af
johnrau <luc.thang@gmail.com>
2024-01-31 21:44:57 +0700

    get name file, need check why only get first name

--- Main.py
+++ Main.py
@@ -43,20 +43,25 @@ def findFiles():
     find_BCTC_box = driver.find_element(By.XPATH,"//li[@id='liTabCongTy5CT']//a")
     find_BCTC_box.click()
     
-    xpathLinks = (By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr//td//a")
+    # xpathLinks = (By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr//td//a")
+    xpathLinks = (By.XPATH, "//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr")
     urls = wait.until(EC.visibility_of_all_elements_located(xpathLinks))
     return urls   
 
-def nameCut():
+def nameCut(dirpath):
     for urlBCTC in findFiles():
-        print('urlBCTC ============= ',urlBCTC)
-        # link_BCTC = urlBCTC.get_attribute("href")
-        orginal_file_name_2 = driver.find_element(By.XPATH,"//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr[position()>1]//td[1]//text()")
+        print('urlBCTC === ',urlBCTC)
+        orginal_file_name_2 = urlBCTC.find_element(By.XPATH,"//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr[position()>1]//td[1]")
+        # Chu y: Neu gap loi "'charmap' codec can't encode character" thi can phai chay command set python encode ve utf-8 nhu sau: export PYTHONIOENCODING=UTF-8
         # orginal_file_name_1 = link_BCTC.split('/')[7]
-        print('orginal_file_name_2 : ',orginal_file_name_2)
-        response = requests.get(orginal_file_name_2)
+        print('orginal_file_name_2 : ',orginal_file_name_2.text)
+        # link_BCTC = urlBCTC.get_attribute("href")
+        link_BCTC = urlBCTC.find_element(By.XPATH,"//div[@id='divDocument']//div[@class='treeview']//table//tbody//tr[position()>1]//td[3]//a").get_attribute("href")
+        print('link_BCTC == ', link_BCTC)
+        response = requests.get(link_BCTC)
 
-        with open(os.path.join(createFolder(mack),orginal_file_name_2), "wb") as pdfFile:
+        print('dirpath == ', dirpath)
+        with open(os.path.join(dirpath,orginal_file_name_2.text), "wb") as pdfFile:
             pdfFile.write(response.content)
 
 def return_to_Homepage():
@@ -68,14 +73,15 @@ for i in range(numDF):
     # print(mack)
 
     #TẠO FOLDER TRONG VÒNG FOR
-    createFolder(mack)
+    dirpath = createFolder(mack)
 
     #TÌM MÃ CHỨNG KHOÁN 'MACK'
     linkMack = searchMack(mack)
     # print(linkMack)
 
-    findFiles()
+    # urlfiledownload = findFiles()
+    # print('urlfiledownload == ',urlfiledownload)
 
-    nameCut()
+    nameCut(dirpath)
 
     return_to_Homepage()
